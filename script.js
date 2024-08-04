82fd4606aae279d5c61cc63d9b76325f3d51c425
arjuunns <singh.arjun2797@gmail.com>
2024-08-05 00:18:19 +0530

    Tailwind Project

---
+++ script.js
@@ -0,0 +1,39 @@
+const navDialog = document.querySelector("#nav-dialog");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+
+function handleMenu() {
+  navDialog.classList.toggle("hidden");
+}
+
+const initialTranslateLTR = -48 * 4;
+const initialTranslateRTL = 36 * 4;
+
+function setupIntersectionObserver(element, isLTR, speed) {
+  const intersectionCallback = (entries) => {
+    const isIntersecting = entries[0].isIntersecting;
+    if (isIntersecting) {
+      document.addEventListener("scroll", scrollHandler);
+    } else {
+      document.removeEventListener("scroll", scrollHandler);
+    }
+  };
+  const intersectionObserver = new IntersectionObserver(intersectionCallback);
+  intersectionObserver.observe(element);
+  function scrollHandler() {
+    const scrollX =
+      (window.innerHeight - element.getBoundingClientRect().top) * speed;
+    let totalTranslate = 0;
+    if (isLTR) {
+      totalTranslate = scrollX + initialTranslateLTR;
+    } else {
+      totalTranslate = -(scrollX + initialTranslateRTL);
+    }
+    element.style.transform = `translateX(${totalTranslate}px)`;
+  }
+}
+
+setupIntersectionObserver(line1, true, 0.15);
+setupIntersectionObserver(line2, false, 0.15);
+setupIntersectionObserver(line3, true, 0.15);
